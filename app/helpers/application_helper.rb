51f0362bf9c8777d829ce40268b1270b76d43628
Yuri Baranov <baranovu@gmail.com>
2017-04-17 23:19:57 +0300

    more prominent navbar menu styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def body_class
+    "#{controller_name} #{controller_name}-#{controller.action_name} #{page_id}"
+  end
+
+  def body_id
+    "#{page_id}"
+  end
+
+  def controller_name
+    controller.controller_path.gsub('/','-')
+  end
+
+  def page_id
+    params['id']
+  end
 end
