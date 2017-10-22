1d68d7202d80e51361087de503d8ba0f87803878
Yuri Baranov <baranovu@gmail.com>
2017-10-23 00:59:14 +0300

    fixed secret_key_base in secrets.yml.enc file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.read_encrypted_secrets = true
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
